63f5fb4cccf00c3eec49fce2ffd8c86beb63e87f
amiraliks07 <amiralik.s110@gmail.com>
2025-05-28 11:17:31 -0700

    Add files via upload

---
+++ jsSample/script.js
@@ -0,0 +1,8 @@
+// hard code
+var number1=12;
+
+if(number1==12){
+    console.log("hello!");
+}else{
+    console.log("whats up");
+}
---
+++ sassSample-v2/scripts/script.js
@@ -0,0 +1,12 @@
+ // public/ global variable
+ var x = document.getElementById("para");
+ function darkMode() {
+
+     x.style.backgroundColor = "Black";
+     x.style.color = "white";
+ }
+ function liteMode() {
+
+     x.style.backgroundColor = "white";
+     x.style.color = "Black";
+ }
